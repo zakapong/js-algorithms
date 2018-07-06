220ad5c52d1bf6ae4c09768404e800dd85c856bd
Zakaria <zakaria.mahmud@ecsolutions.se>
2018-07-06 10:36:33 +0200

    Reverse Array algo

---
+++ Reverse Array/reverseArray.js
@@ -0,0 +1,15 @@
+function reverseArrayInPlace(arr){
+    //reverse array
+    //returned reverse array
+ 
+  for (var i= 0; i< arr.length/2; i++) {
+   var tempVar= arr[i];
+     arr[i] = arr[arr.length -1 -i];
+     arr[arr.length -1 -i] = tempVar;
+
+  }
+  return arr;
+}
+
+
+console.log(reverseArrayInPlace([1,2,3,4,5]));
